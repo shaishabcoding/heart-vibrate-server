9b9c66235c43c3f5ba3460d2f6e2043d20c486a0
shaishabcoding <shaishabchandrashil@gmail.com>
2025-02-17 10:47:21 +0600

    refactor: consolidate AuthServices methods and improve validation schema structure

--- src/app/modules/auth/Auth.service.ts
+++ src/app/modules/auth/Auth.service.ts
@@ -8,146 +8,134 @@ import ApiError from '../../../errors/ApiError';
 import { sendEmail } from '../../../helpers/sendMail';
 import config from '../../../config';
 
-const loginUser = async ({
-  email,
-  password,
-}: {
-  email: string;
-  password: string;
-}) => {
-  const user = await User.findOne({
-    email,
-  }).select('+password');
-
-  if (!user) {
-    throw new ApiError(StatusCodes.NOT_FOUND, 'User not found!');
-  }
-
-  if (user.status !== 'ACTIVE') {
-    throw new ApiError(
-      StatusCodes.FORBIDDEN,
-      'Account is not active. Please contact support.',
-    );
-  }
-
-  const isPasswordValid = await bcrypt.compare(password, user.password);
-  if (!isPasswordValid) {
-    throw new ApiError(StatusCodes.UNAUTHORIZED, 'Incorrect password!');
-  }
-
-  const {
-    _id,
-    gender,
-    name: { firstName, lastName },
-    role,
-    avatar,
-  } = user.toJSON();
-
-  const partialUser: Partial<TUser> = {
-    _id,
-    email,
-    gender,
-    name: { firstName, lastName },
-    role,
-    avatar,
-  };
-
-  const jwtPayload = {
-    email,
-  };
-
-  const accessToken = createToken(jwtPayload, 'access');
-
-  const refreshToken = createToken(jwtPayload, 'refresh');
-
-  return { accessToken, user: partialUser, refreshToken };
-};
-
-const refreshToken = async (token: string) => {
-  if (!token) {
-    throw new ApiError(StatusCodes.UNAUTHORIZED, 'Access Denied!');
-  }
-
-  const { email } = verifyToken(token, 'refresh');
-
-  const user = await User.findOne({
-    email,
-  });
-
-  if (!user) {
-    throw new ApiError(StatusCodes.NOT_FOUND, 'User not found!');
-  }
-
-  if (user.status !== 'ACTIVE') {
-    throw new ApiError(
-      StatusCodes.FORBIDDEN,
-      'Account is not active. Please contact support.',
-    );
-  }
-
-  const jwtPayload = {
-    email,
-  };
-
-  const accessToken = createToken(jwtPayload, 'access');
-
-  return { accessToken };
-};
-
-const changePassword = async (
-  user: TUser,
-  {
-    newPassword,
-    oldPassword,
-  }: {
-    newPassword: string;
-    oldPassword: string;
-  },
-) => {
-  const isPasswordValid = await bcrypt.compare(oldPassword, user.password);
-  if (!isPasswordValid) {
-    throw new ApiError(StatusCodes.UNAUTHORIZED, 'Incorrect password!');
-  }
-
-  newPassword = await bcrypt.hash(
-    newPassword,
-    +(config.bcrypt_salt_rounds as string),
-  );
-
-  await User.updateOne(
-    {
-      email: user.email,
-    },
-    {
-      password: newPassword,
-    },
-  );
-};
-
-const forgetPassword = async ({ email }: TUser) => {
-  const jwtPayload = {
-    email,
-  };
-
-  const resetToken = createToken(jwtPayload, 'reset');
-
-  await sendEmail(email, 'Password Reset Request', makeResetBody(resetToken));
-};
-
-const resetPassword = async ({ email }: TUser) => {
-  const jwtPayload = {
-    email,
-  };
-
-  const resetToken = createToken(jwtPayload, 'reset');
-
-  await sendEmail(email, 'Password Reset Request', makeResetBody(resetToken));
-};
-
 export const AuthServices = {
-  loginUser,
-  changePassword,
-  forgetPassword,
-  resetPassword,
-  refreshToken,
+  async loginUser({ email, password }: { email: string; password: string }) {
+    const user = await User.findOne({
+      email,
+    }).select('+password');
+
+    if (!user) {
+      throw new ApiError(StatusCodes.NOT_FOUND, 'User not found!');
+    }
+
+    if (user.status !== 'ACTIVE') {
+      throw new ApiError(
+        StatusCodes.FORBIDDEN,
+        'Account is not active. Please contact support.',
+      );
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      throw new ApiError(StatusCodes.UNAUTHORIZED, 'Incorrect password!');
+    }
+
+    const {
+      _id,
+      gender,
+      name: { firstName, lastName },
+      role,
+      avatar,
+    } = user.toJSON();
+
+    const partialUser: Partial<TUser> = {
+      _id,
+      email,
+      gender,
+      name: { firstName, lastName },
+      role,
+      avatar,
+    };
+
+    const jwtPayload = {
+      email,
+    };
+
+    const accessToken = createToken(jwtPayload, 'access');
+
+    const refreshToken = createToken(jwtPayload, 'refresh');
+
+    return { accessToken, user: partialUser, refreshToken };
+  },
+
+  async changePassword(
+    user: TUser,
+    {
+      newPassword,
+      oldPassword,
+    }: {
+      newPassword: string;
+      oldPassword: string;
+    },
+  ) {
+    const isPasswordValid = await bcrypt.compare(oldPassword, user.password);
+    if (!isPasswordValid) {
+      throw new ApiError(StatusCodes.UNAUTHORIZED, 'Incorrect password!');
+    }
+
+    newPassword = await bcrypt.hash(
+      newPassword,
+      +(config.bcrypt_salt_rounds as string),
+    );
+
+    await User.updateOne(
+      {
+        email: user.email,
+      },
+      {
+        password: newPassword,
+      },
+    );
+  },
+
+  async forgetPassword({ email }: TUser) {
+    const jwtPayload = {
+      email,
+    };
+
+    const resetToken = createToken(jwtPayload, 'reset');
+
+    await sendEmail(email, 'Password Reset Request', makeResetBody(resetToken));
+  },
+
+  async resetPassword({ email }: TUser) {
+    const jwtPayload = {
+      email,
+    };
+
+    const resetToken = createToken(jwtPayload, 'reset');
+
+    await sendEmail(email, 'Password Reset Request', makeResetBody(resetToken));
+  },
+
+  async refreshToken(token: string) {
+    if (!token) {
+      throw new ApiError(StatusCodes.UNAUTHORIZED, 'Access Denied!');
+    }
+
+    const { email } = verifyToken(token, 'refresh');
+
+    const user = await User.findOne({
+      email,
+    });
+
+    if (!user) {
+      throw new ApiError(StatusCodes.NOT_FOUND, 'User not found!');
+    }
+
+    if (user.status !== 'ACTIVE') {
+      throw new ApiError(
+        StatusCodes.FORBIDDEN,
+        'Account is not active. Please contact support.',
+      );
+    }
+
+    const jwtPayload = {
+      email,
+    };
+
+    const accessToken = createToken(jwtPayload, 'access');
+
+    return { accessToken };
+  },
 };
--- src/app/modules/auth/Auth.validation.ts
+++ src/app/modules/auth/Auth.validation.ts
@@ -1,31 +1,24 @@
 import { z } from 'zod';
 
-const passwordChangeValidationSchema = z.object({
-  body: z.object({
-    oldPassword: z
-      .string()
-      .min(1, 'Old Password is required')
-      .min(6, 'Old Password must be at least 6 characters long'),
-    newPassword: z
-      .string()
-      .min(1, 'New Password is required')
-      .min(6, 'New Password must be at least 6 characters long'),
-  }),
-});
-
-const refreshTokenValidationSchema = z.object({
-  cookies: z.object({
-    refreshToken: z.string({
-      required_error: 'refreshToken is missing',
+export const AuthValidation = {
+  passwordChangeValidationSchema: z.object({
+    body: z.object({
+      oldPassword: z
+        .string()
+        .min(1, 'Old Password is required')
+        .min(6, 'Old Password must be at least 6 characters long'),
+      newPassword: z
+        .string()
+        .min(1, 'New Password is required')
+        .min(6, 'New Password must be at least 6 characters long'),
     }),
   }),
-});
 
-export type TPasswordChange = z.infer<
-  typeof passwordChangeValidationSchema.shape.body
->;
-
-export const AuthValidation = {
-  passwordChangeValidationSchema,
-  refreshTokenValidationSchema,
+  refreshTokenValidationSchema: z.object({
+    cookies: z.object({
+      refreshToken: z.string({
+        required_error: 'refreshToken is missing',
+      }),
+    }),
+  }),
 };

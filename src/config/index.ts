8535c4fc8c7220d1234ac97e72dc47c7c3e7429f
shaishabcoding <shaishabchandrashil@gmail.com>
2025-07-09 17:32:17 +0600

    clear up

--- src/config/index.ts
+++ src/config/index.ts
@@ -9,17 +9,11 @@ export default {
   node_env: process.env.NODE_ENV,
   port: process.env.PORT,
   bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
-  stripe_api_secret: process.env.STRIPE_SECRET_KEY,
-  google_maps: process.env.GOOGLE_MAPS,
   jwt: {
     jwt_secret: process.env.JWT_SECRET,
     jwt_expire_in: process.env.JWT_EXPIRE_IN,
     jwtRefreshSecret: process.env.JWT_REFRESH_SECRET,
     jwtRefreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN,
-  },
-  payment: {
-    stripe_secret_key: process.env.STRIPE_SECRET_KEY,
-    stripe_webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
   },
   email: {
     from: process.env.EMAIL_FROM,

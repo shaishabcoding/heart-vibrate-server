54ce28082d525148bb973ee009a6762a6b481acf
shaishab316 <shaishabchandrashil@gmail.com>
2026-03-24 18:00:56 +0600

    feat(config): add ALLOWED_ORIGINS to environment schema and implement CORS in main.ts

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -6,6 +6,16 @@ const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('development'),
   JWT_SECRET: z.string('JWT_SECRET is required').default('your-super-secret-key'),
   JWT_EXPIRES_IN: z.string('JWT_EXPIRES_IN is required').default('7d'),
+  ALLOWED_ORIGINS: z.preprocess(
+    (val) => {
+      if (val === '*') return '*';
+      if (typeof val === 'string') {
+        return val.split(',').map((s) => s.trim());
+      }
+      return val;
+    },
+    z.union([z.literal('*'), z.array(z.url('Each origin must be a valid URL')).nonempty()]),
+  ),
 });
 
 export const validate = (config: Record<string, unknown>) => {
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import chalk from 'chalk';
+import compression from 'compression';
+import { NextFunction, Request, Response } from 'express';
+import helmet from 'helmet';
 import { ZodValidationPipe } from 'nestjs-zod';
 import { AppModule } from './app.module';
 import { setupApiDocs } from './common/config/api-docs.config';
@@ -11,11 +14,38 @@ import type { Env } from './config/app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService<Env, true>);
 
+  //? security headers
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    if (req.path.startsWith('/docs') || req.path.startsWith('/ws-docs')) {
+      helmet({ contentSecurityPolicy: false })(req, res, next);
+    } else {
+      helmet()(req, res, next);
+    }
+  });
+
+  //? cors
+  app.enableCors({
+    origin: config.get('ALLOWED_ORIGINS', { infer: true }), // e.g 'http://localhost:3000'
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    credentials: true,
+  });
+
+  //? gzip compression
+  app.use(compression());
+
+  //? global prefix
+  app.setGlobalPrefix('api/v1');
+
+  //? global pipes — zod validation
   app.useGlobalPipes(new ZodValidationPipe());
+
+  //? global interceptors
   app.useGlobalInterceptors(new ResponseInterceptor());
 
-  const config = app.get(ConfigService<Env, true>);
+  //? global exception filter
+  // app.useGlobalFilters(new GlobalExceptionFilter()); //* TODO: implement a global exception filter to handle all exceptions in a consistent way
 
   setupApiDocs(app);
 

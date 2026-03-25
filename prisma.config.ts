9e0dcea451f0c459411467ce02679523c4ff7bd7
shaishab316 <shaishabchandrashil@gmail.com>
2026-03-25 10:02:12 +0600

    feat(prisma): update schema path and add user model definition

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
-  schema: 'src/modules',
+  schema: 'prisma/schema',
   migrations: {
     path: 'prisma/migrations',
   },
